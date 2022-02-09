304f8837b9d9aff72988befae620314d8b557564
noahlopcor <99145902+noahlopcor@users.noreply.github.com>
2022-02-09 09:09:57 +0100

    holiwiiiiis

--- main.ts
+++ main.ts
@@ -1,1 +1,60 @@
-
+radio.onReceivedNumber(function (receivedNumber) {
+    jugada_2 = randint(1, 3)
+    if (jugada_2 == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (jugada_2 == 2) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            # # . # #
+            # # . # #
+            `)
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    jugada = randint(1, 3)
+    radio.sendNumber(jugada)
+    if (jugada == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (jugada == 2) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            # # . # #
+            # # . # #
+            `)
+    }
+})
+let jugada = 0
+let jugada_2 = 0
+radio.setGroup(7)
